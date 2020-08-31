856fe49aed37c7ba7dd894ea918678a8fe70d474
Benjamin Rosen <ben@classy.name>
2020-08-31 01:43:41 -0400

    mock enroll form

--- src/bits/customer/Enroll.jsx
+++ src/bits/customer/Enroll.jsx
@@ -1,9 +1,64 @@
-import PropTypes from "prop-types";
-import React from "react";
+import {
+  Checkbox,
+  Form,
+  Grid,
+  Header,
+  Segment,
+  TextArea,
+} from "semantic-ui-react";
+import React, { useState } from "react";
 
+// TODO: enroll student
+// TODO: focus on first blank required field when user views form
+// TODO: set form state (loading, error, warning, success)
+// TODO: useState for each field value
 const Enroll = (props) => {
-  // TODO: enroll student
-  return <p>Enroll</p>;
+  const [loading, setLoading] = useState(false);
+  return (
+    <Grid container padded>
+      <Grid.Column align="center">
+        <Segment align="left" compact raised padded="very">
+          <Header>Enroll a new student</Header>
+          <Form
+            loading={loading}
+            onSubmit={() => {
+              setLoading(true);
+              // TODO: validate form
+              // TODO: use form data to create student in graphql api
+              // TODO: redirect to new student page on success
+              console.log("submit");
+            }}
+          >
+            <Form.Field id="form-input-control-first-name" required>
+              <label>First name</label>
+              <input placeholder="First name"></input>
+            </Form.Field>
+            <Form.Field id="form-input-control-last-name" required>
+              <label>Last name</label>
+              <input placeholder="Last name"></input>
+            </Form.Field>
+            <Form.Field id="form-input-control-birthday" required>
+              <label>Birthday</label>
+              <input type="date"></input>
+            </Form.Field>
+            <Form.Field
+              control={TextArea}
+              id="form-textarea-control-about"
+              label="About"
+              placeholder="Anything you want us to know?"
+            />
+            <Form.Field
+              control={Checkbox}
+              id="form-checkbox-control-agreement"
+              label="I agree to the Terms and Conditions"
+              required
+            />
+            <Form.Button content="Submit" fluid primary />
+          </Form>
+        </Segment>
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default Enroll;
