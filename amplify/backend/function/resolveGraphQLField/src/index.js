6f825ce03136fb335a15003010b7dd253867eda4
Benjamin Rosen <ben@classy.name>
2020-09-05 00:29:18 -0400

    mock graphql field resolver

--- amplify/backend/function/resolveGraphQLField/src/index.js
+++ amplify/backend/function/resolveGraphQLField/src/index.js
@@ -2,11 +2,40 @@
 	AUTH_CLIENT936872C9_USERPOOLID
 Amplify Params - DO NOT EDIT */
 
+const { CognitoIdentityServiceProvider } = require("aws-sdk");
+const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
+
+const COGNITO_USERPOOL_ID = process.env.AUTH_CLIENT936872C9_USERPOOLID;
+if (!COGNITO_USERPOOL_ID) {
+  throw new Error(
+    `Function requires environment variable: 'COGNITO_USERPOOL_ID'`
+  );
+}
+
+async function getUser(username) {
+  try {
+    return await cognitoIdentityServiceProvider
+      .adminGetUser({ UserPoolId: COGNITO_USERPOOL_ID, Username: username })
+      .promise();
+  } catch (e) {
+    throw new Error(`NOT FOUND`);
+  }
+}
+
+const resolvers = {
+  StudentAccount: {
+    customer: (event) => getUser(event.source.customerId),
+    student: (event) => getUser(event.source.studentId),
+  },
+};
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  const typeHandler = resolvers[event.typeName];
+  if (typeHandler) {
+    const resolver = typeHandler[event.fieldName];
+    if (resolver) {
+      return await resolver(event);
+    }
+  }
+  throw new Error("Resolver not found.");
 };
