14062f426be994ca564f7949c1606384cf8d9058
Benjamin Rosen <ben@classy.name>
2020-09-06 00:11:24 -0400

    override StudentAccount mutations with lambda resolvers

---
+++ amplify/backend/function/createStudentAccount/src/index.js
@@ -0,0 +1,12 @@
+/* Amplify Params - DO NOT EDIT
+	AUTH_CLIENT936872C9_USERPOOLID
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
