dc1f4f816b8adddb80e827be41ac96cffcc1e986
Benjamin Rosen <ben@classy.name>
2020-09-15 01:00:44 -0400

    create student account

--- amplify/backend/function/createStudentAccount/src/index.js
+++ amplify/backend/function/createStudentAccount/src/index.js
@@ -1,45 +1,63 @@
 /* Amplify Params - DO NOT EDIT
+	API_CLIENT_GRAPHQLAPIIDOUTPUT
+	API_CLIENT_STUDENTACCOUNTTABLE_ARN
+	API_CLIENT_STUDENTACCOUNTTABLE_NAME
 	AUTH_CLIENT936872C9_USERPOOLID
 	ENV
 	REGION
 Amplify Params - DO NOT EDIT */
 
-const { CognitoIdentityServiceProvider } = require("aws-sdk");
+const AWS = require("aws-sdk");
 
 const COGNITO_USERPOOL_ID = process.env.AUTH_CLIENT936872C9_USERPOOLID;
 if (!COGNITO_USERPOOL_ID) {
   throw new Error(
-    `Function requires environment variable: 'COGNITO_USERPOOL_ID'`
+    `Function requires environment variable: 'AUTH_CLIENT936872C9_USERPOOLID'`
   );
 }
 
-const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
-
-async function generateUsername({ givenName, familyName }) {
-  const prefix = (givenName + familyName).toLowerCase();
-  let suffix = 0;
-  const username = () => prefix + suffix;
-  const claimedUsernames = await cognitoIdentityServiceProvider
-    .listUsers({
-      UserPoolId: COGNITO_USERPOOL_ID,
-      AttributesToGet: ["username"],
-      Filter: `username ^= "${prefix}"`,
-    })
-    .promise().Users;
-  while (claimedUsernames && claimedUsernames.includes(username())) {
-    suffix++;
-  }
-  return username();
+const STUDENT_ACCOUNT_TABLE_NAME =
+  process.env.API_CLIENT_STUDENTACCOUNTTABLE_NAME;
+if (!STUDENT_ACCOUNT_TABLE_NAME) {
+  throw new Error(
+    `Function requires environment variable: 'API_CLIENT_STUDENTACCOUNTTABLE_NAME'`
+  );
 }
 
-async function createStudentUser({
-  username,
-  birthdate,
-  email,
-  familyName,
-  givenName,
-}) {
-  const user = await cognitoIdentityServiceProvider
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+  const args = event.arguments.input;
+  const username = await getUsername(args.givenName, args.familyName);
+  const user = await createUser(
+    username,
+    args.birthdate,
+    args.email,
+    args.givenName,
+    args.familyName
+  );
+  await groupUser(username, "Students");
+  const account = {
+    id: event.accountId,
+    customerUsername: event.identity.username,
+    studentUsername: username,
+  };
+  await createAccount(account);
+  return account;
+};
+
+function createAccount(item) {
+  return documentClient
+    .put({
+      TableName: STUDENT_ACCOUNT_TABLE_NAME,
+      Item: item,
+    })
+    .promise();
+}
+
+function createUser(username, birthdate, email, givenName, familyName) {
+  return cognitoIdentityServiceProvider
     .adminCreateUser({
       UserPoolId: COGNITO_USERPOOL_ID,
       Username: username,
@@ -52,22 +70,32 @@ async function createStudentUser({
       ],
     })
     .promise();
-  console.log(user);
+}
+
+// TODO: fix "User account already exists" error
+async function getUsername(givenName, familyName) {
+  const prefix = (givenName + familyName).toLowerCase();
+  let suffix = 0;
+  const concatenate = () => prefix + suffix;
   const response = await cognitoIdentityServiceProvider
+    .listUsers({
+      UserPoolId: COGNITO_USERPOOL_ID,
+      Filter: `username ^= \"${prefix}\"`,
+    })
+    .promise();
+  const claimedUsernames = response.Users;
+  while (claimedUsernames && claimedUsernames.includes(concatenate())) {
+    suffix++;
+  }
+  return concatenate();
+}
+
+function groupUser(username, group) {
+  return cognitoIdentityServiceProvider
     .adminAddUserToGroup({
-      GroupName: "Students", // TODO: extract group names to constants or something
+      GroupName: group,
       UserPoolId: COGNITO_USERPOOL_ID,
       Username: username,
     })
     .promise();
-  console.log(response);
 }
-
-exports.handler = async (event) => {
-  const props = event.arguments.input;
-  const username = await generateUsername(props);
-  const user = await createStudentUser({ username, ...props });
-  // TODO: create new cognito user in students group using username, email, phone
-  // TODO: compose and add new StudentAccount item to StudentAccount table
-  // TODO: return new StudentAccount item
-};
