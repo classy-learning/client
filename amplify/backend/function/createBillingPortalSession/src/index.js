50ea29b3ab1306e8a1d099b0b937949027c6664a
Benjamin Rosen <ben@classy.name>
2020-09-20 16:55:14 -0400

    get customer username using sub

--- amplify/backend/function/createBillingPortalSession/src/index.js
+++ amplify/backend/function/createBillingPortalSession/src/index.js
@@ -6,31 +6,32 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
-// TODO: get sub for cognito user from event
-// TODO: get username from cognito using sub
 // TODO: get stripe customer id from gqlapi using username
 // TODO: if no stripe customer exists, create one using gql api mutation
 // TODO: use stripe customer id to return new billing portal session
 
 // TODO: create createStripeCustomer mutation that invokes a lambda function to create a customer
 
-const STRIPE_API_KEY_SECRET_ID = "stripe-api-key";
-const RETURN_URL = "https://classy.name/account";
-
 const stripe = require("stripe");
 const AWS = require("aws-sdk");
 
+const STRIPE_API_KEY_SECRET_ID = "stripe-api-key";
+const RETURN_URL = "https://classy.name/account";
+
+const COGNITO_USERPOOL_ID = process.env.AUTH_CLIENT01A53182_USERPOOLID;
+if (!COGNITO_USERPOOL_ID) {
+  throw new Error(
+    `Function requires environment variable: 'AUTH_CLIENT01A53182_USERPOOLID'`
+  );
+}
+
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
 const secretsManager = new AWS.SecretsManager();
 
 exports.handler = async (event) => {
-  const secret = await secretsManager
-    .getSecretValue({ SecretId: STRIPE_API_KEY_SECRET_ID })
-    .promise();
-  const stripeApiKey = JSON.parse(secret.SecretString)[
-    STRIPE_API_KEY_SECRET_ID
-  ];
-  const Stripe = stripe(stripeApiKey);
-
+  const Stripe = await configureStripe();
+  const username = await getUsername(event);
+  console.log(username);
   return {
     statusCode: 200,
     headers: {
@@ -39,3 +40,33 @@ exports.handler = async (event) => {
     body: JSON.stringify("Hello from Lambda!"),
   };
 };
+
+function configureStripe() {
+  return secretsManager
+    .getSecretValue({ SecretId: STRIPE_API_KEY_SECRET_ID })
+    .promise()
+    .then((secret) => {
+      const stripeApiKey = JSON.parse(secret.SecretString)[
+        STRIPE_API_KEY_SECRET_ID
+      ];
+      return stripe(stripeApiKey);
+    });
+}
+
+function getSub(event) {
+  const provider = event.requestContext.identity.cognitoAuthenticationProvider;
+  return provider.substring(provider.lastIndexOf(":") + 1);
+}
+
+function getUsername(event) {
+  const sub = getSub(event);
+  return cognitoIdentityServiceProvider
+    .listUsers({
+      UserPoolId: COGNITO_USERPOOL_ID,
+      Filter: `sub = \"${sub}\"`,
+    })
+    .promise()
+    .then((data) => {
+      return data.Users[0].Username;
+    });
+}
--- amplify/backend/function/createStripeCustomer/src/index.js
+++ amplify/backend/function/createStripeCustomer/src/index.js
@@ -4,11 +4,48 @@
 	API_CLIENT_STRIPECUSTOMERTABLE_NAME
 Amplify Params - DO NOT EDIT */
 
+const stripe = require("stripe");
+const AWS = require("aws-sdk");
+
+const STRIPE_API_KEY_SECRET_ID = "stripe-api-key";
+
+const STRIPE_CUSTOMER_TABLE_NAME =
+  process.env.API_CLIENT_STRIPECUSTOMERTABLE_NAME;
+if (!STRIPE_CUSTOMER_TABLE_NAME) {
+  throw new Error(
+    `Function requires environment variable: 'API_CLIENT_STRIPECUSTOMERTABLE_NAME'`
+  );
+}
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  const Stripe = await configureStripe();
+
+  console.log(event);
+
+  // TODO: extract customerUsername from event
+  // TODO: create stripe customer & extract stripe customerId
+  // TODO: create dynamodb record with stripe customerId and customerUsername
+
+  const stripeCustomer = { id: event.stripeCustomerId };
+
+  // await documentClient.put({
+  //   TableName: STRIPE_CUSTOMER_TABLE_NAME,
+  //   Item: stripeCustomer,
+  // });
+
+  return stripeCustomer;
 };
+
+function configureStripe() {
+  return secretsManager
+    .getSecretValue({ SecretId: STRIPE_API_KEY_SECRET_ID })
+    .promise()
+    .then((secret) => {
+      const stripeApiKey = JSON.parse(secret.SecretString)[
+        STRIPE_API_KEY_SECRET_ID
+      ];
+      return stripe(stripeApiKey);
+    });
+}
