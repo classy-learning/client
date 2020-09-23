47fb7162f47a2d140a8fd4c845f93144223212b7
Benjamin Rosen <ben@classy.name>
2020-09-23 15:53:41 -0400

    install stripe elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import AppRouter from "bits/AppRouter";
 import AuthWrapper from "bits/AuthWrapper";
+import { Elements } from "@stripe/react-stripe-js";
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+
+const PUBLISHABLE_STRIPE_KEY =
+  "pk_test_51H6rIkIw1gARdZqqtmeFOSI8nsqdyHQAtH2XRAkSMSkzCz5AZCPwsUU1BVPqFOa8uwrFihNrMjEkAC7NkEHI7gsF00MxWZlulW";
+
+const stripePromise = loadStripe(PUBLISHABLE_STRIPE_KEY);
 
 function App() {
   return (
-    <AuthWrapper>
-      <AppRouter></AppRouter>
-    </AuthWrapper>
+    <Elements stripe={stripePromise}>
+      <AuthWrapper>
+        <AppRouter></AppRouter>
+      </AuthWrapper>
+    </Elements>
   );
 }
 
