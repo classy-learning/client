2ede5f6f31d9c3d2f93b496e142c5d271d346c3b
Benjamin Rosen <ben@classy.name>
2020-09-28 14:40:04 -0400

    refactor enrollment steps

--- src/bits/customer/EnrollmentSteps.jsx
+++ src/bits/customer/EnrollmentSteps.jsx
@@ -1,44 +1,36 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 
-import PropTypes from "prop-types";
 import { Step } from "semantic-ui-react";
 import StudentContext from "bits/customer/StudentContext";
-
-const BILLING_STEP = "Billing";
-const BOOKING_STEP = "Booking";
-const ENROLLMENT_STEP = "Enrollment";
 
 const EnrollmentSteps = (props) => {
   const student = useContext(StudentContext);
 
-  const [completedSteps, setCompletedSteps] = useState([ENROLLMENT_STEP]);
-
-  const steps = [
-    {
-      title: ENROLLMENT_STEP,
-      description: `Register ${student.givenName}'s account.`,
-      props: {},
-    },
-    {
-      title: BILLING_STEP,
-      description: `Start a subscription plan.`,
-      props: { active: true },
-    },
-    {
-      title: BOOKING_STEP,
-      description: `Schedule ${student.givenName}'s first lesson.`,
-      props: {},
-    },
-  ];
-
   return (
-    <Step.Group ordered size="mini" widths={steps.length}>
-      {steps.map((step) => (
-        <Step
-          {...step.props}
-          completed={completedSteps.includes(step.title)}
-          key={step.title}
-        >
+    <Step.Group ordered size="mini" widths={3}>
+      {[
+        {
+          active: false,
+          completed: true,
+          description: `Register ${student.givenName}'s account.`,
+          key: "Enrollment",
+          title: "Enrollment",
+        },
+        {
+          active: !student.subscriptionStatus,
+          completed: student.subscriptionStatus,
+          description: `Start a subscription plan.`,
+          key: "Billing",
+          title: "Billing",
+        },
+        {
+          active: student.subscriptionStatus == true,
+          description: `Schedule ${student.givenName}'s first lesson.`,
+          key: "Booking",
+          title: "Booking",
+        },
+      ].map((step) => (
+        <Step {...step}>
           <Step.Content>
             <Step.Title>{step.title}</Step.Title>
             <Step.Description>{step.description}</Step.Description>
