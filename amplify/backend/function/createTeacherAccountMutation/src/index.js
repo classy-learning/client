918a42a334caf3d406af3d91f76df47d1cb09565
Benjamin Rosen <ben@classy.name>
2020-10-01 01:12:42 -0400

    push teacher, topic, resource, and related resources

--- amplify/backend/function/createTeacherAccountMutation/src/index.js
+++ amplify/backend/function/createTeacherAccountMutation/src/index.js
@@ -1,19 +1,54 @@
 /* Amplify Params - DO NOT EDIT
-	API_CLIENT_GRAPHQLAPIENDPOINTOUTPUT
-	API_CLIENT_GRAPHQLAPIIDOUTPUT
 	ENV
+	FUNCTION_EXECUTEGRAPHQLOPERATION_NAME
 	REGION
 Amplify Params - DO NOT EDIT */
 
+const AWS = require("aws-sdk");
+
+const EXECUTE_GQL_OPERATION_FUNCTION_NAME =
+  process.env.FUNCTION_EXECUTEGRAPHQLOPERATION_NAME;
+if (!EXECUTE_GQL_OPERATION_FUNCTION_NAME) {
+  throw new Error(
+    `Function requires environment variable: 'FUNCTION_EXECUTEGRAPHQLOPERATION_NAME'`
+  );
+}
+
+const lambda = new AWS.Lambda();
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  return createTeacherAccount(event);
 };
+
+function createTeacherAccount(username) {
+  return executeGraphQLOperation(
+    `mutation CreateTeacherAccount(
+      $input: CreateTeacherAccountInput!
+      $condition: ModelTeacherAccountConditionInput
+    ) {
+      createTeacherAccount(input: $input, condition: $condition) {
+        teacherUsername
+      }
+    }`,
+    "CreateTeacherAccount",
+    { input: { teacherUsername: username } }
+  ).then((response) => {
+    return response.data.createTeacherAccount;
+  });
+}
+
+function executeGraphQLOperation(operation, operationName, item) {
+  return lambda
+    .invoke({
+      FunctionName: EXECUTE_GQL_OPERATION_FUNCTION_NAME,
+      Payload: JSON.stringify({
+        operation: operation,
+        operationName: operationName,
+        item: item,
+      }),
+    })
+    .promise()
+    .then((response) => {
+      return JSON.parse(response.Payload);
+    });
+}
