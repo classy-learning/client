669eb898dc556c9553824f76b5979cd35c2eaa96
Benjamin Rosen <ben@classy.name>
2020-10-02 21:31:23 -0400

    mock timekit lambdas and add timekit app key to secrets manager

--- amplify/backend/function/getTimekitBookingById/src/index.js
+++ amplify/backend/function/getTimekitBookingById/src/index.js
@@ -1,14 +1,40 @@
+const AWS = require("aws-sdk");
+const timekit = require("timekit-sdk");
 
+const TIMEKIT_API_KEY_SECRET_ID = "timekit-app-key";
+
+const secretsManager = new AWS.SecretsManager();
 
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  await configureTimekit();
+  return getBookingByBookingId(event);
 };
+
+function configureTimekit() {
+  return secretsManager
+    .getSecretValue({ SecretId: TIMEKIT_API_KEY_SECRET_ID })
+    .promise()
+    .then((secret) => {
+      const timekitApiKey = JSON.parse(secret.SecretString)[
+        TIMEKIT_API_KEY_SECRET_ID
+      ];
+      timekit.configure({ appKey: timekitApiKey });
+    });
+}
+
+function getBookingByBookingId(bookingId) {
+  console.log(bookingId);
+  if (!bookingId) {
+    return null;
+  }
+  return timekit
+    .getBooking({ id: bookingId })
+    .then((response) => {
+      console.log(response);
+      return response.data;
+    })
+    .catch((response) => {
+      console.log(response);
+      console.log(response);
+    });
+}
--- amplify/backend/function/getTimekitResourceById/src/index.js
+++ amplify/backend/function/getTimekitResourceById/src/index.js
@@ -1,12 +1,40 @@
+const AWS = require("aws-sdk");
+const timekit = require("timekit-sdk");
+
+const TIMEKIT_API_KEY_SECRET_ID = "timekit-app-key";
+
+const secretsManager = new AWS.SecretsManager();
+
 exports.handler = async (event) => {
-  // TODO: implement
-  const response = {
-    statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*"
-    //  },
-    body: JSON.stringify("Hello from Lambda!"),
-  };
-  return response;
+  await configureTimekit();
+  return getResourceByResourceId(event);
 };
+
+function configureTimekit() {
+  return secretsManager
+    .getSecretValue({ SecretId: TIMEKIT_API_KEY_SECRET_ID })
+    .promise()
+    .then((secret) => {
+      const timekitApiKey = JSON.parse(secret.SecretString)[
+        TIMEKIT_API_KEY_SECRET_ID
+      ];
+      timekit.configure({ appKey: timekitApiKey });
+    });
+}
+
+function getResourceByResourceId(resourceId) {
+  console.log(resourceId);
+  if (!resourceId) {
+    return null;
+  }
+  return timekit
+    .getResource({ id: resourceId })
+    .then((response) => {
+      console.log(response);
+      return response.data;
+    })
+    .catch((response) => {
+      console.log(response);
+      console.log(response);
+    });
+}
