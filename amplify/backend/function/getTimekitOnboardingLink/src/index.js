2df8dce2b9d528715644467f6034a34d1b6c2888
Benjamin Rosen <ben@classy.name>
2020-10-05 11:21:54 -0400

    remove update lesson lambdas

--- amplify/backend/function/getTimekitOnboardingLink/src/index.js
+++ amplify/backend/function/getTimekitOnboardingLink/src/index.js
@@ -17,17 +17,26 @@ if (!HASH_TEACHER_ACCOUNT_ID_FUNCTION_NAME) {
 const lambda = new AWS.Lambda();
 
 exports.handler = async (event) => {
+  console.log(event);
   // TODO: get teacherAccountId from event
-  const teacherAccountId = "";
-  const teacherAccountIdHash = await hashTeacherAccountId(teacherAccountId);
-  return getTimekitOnboardingLink(teacherAccountIdHash);
+  // const teacherAccountId = "";
+  // const teacherAccountIdHash = await hashTeacherAccountId(teacherAccountId);
+  // const timekitOnboardingLink = getTimekitOnboardingLink(teacherAccountIdHash);
+  // TODO: return timekitOnboardingLink
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify("Hello from Lambda!"),
+  };
 };
 
 function getTimekitOnboardingLink(teacherAccountIdHash) {
   // TODO: create timekit onboarding link
-  const timekitOnboardingUrl = "";
-  const confirmResourceEndpoint = "";
-  return timekitOnboardingUrl + confirmResourceEndpoint + teacherAccountIdHash;
+  const timekitBaseUrl = "";
+  const endpointUrl = "";
+  return timekitBaseUrl + endpointUrl + teacherAccountIdHash;
 }
 
 function hashTeacherAccountId(teacherAccountId) {
